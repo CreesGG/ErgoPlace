c62ca9074812021dfc50ebabfabe6c1a9b4d5257
CreesGG <jeroen_wiersma1995@live.nl>
2020-01-21 20:46:36 +0100

    1 file modified

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      bodyLength:125;
+      bodyLength:125
     };
   }
+  handleChange(event) {
+  this.setState({bodyLength: event.target.value});
+}
   render() {
     return (
       <div>
         <h1>Ergonomische werkplek</h1>
         <h3>Bereken nu een ergonomische hoogte voor je bureau en stoel</h3>
+        <p>{this.state.bodyLength}</p>
         <input type="range" name="points" min="0" onChange={this.handleChange}value={this.state.bodyLength}  max="250"/>
       </div>
     );
