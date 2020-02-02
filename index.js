edc82a00fede0323310ab2f81a0ebd1e47d61fed
CreesGG <jeroen_wiersma1995@live.nl>
2020-02-02 19:50:45 +0100

    1 file modified

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ class App extends Component {
       <div>
         <h1>Ergonomische werkplek</h1>
         <h3>Bereken nu een ergonomische hoogte voor je bureau en stoel</h3>
-        <p>{this.state.bodyLength} CM</p> <p>{this.state.deskHeight} CM</p>
-        <p>{this.state.chairHeight} CM</p>
+        <p>Lichaamslengte:{this.state.bodyLength} CM</p> <p>Bureauhoogte:{this.state.deskHeight} CM</p>
+        <p>Stoelhoogte: {this.state.chairHeight} CM</p>
         <input className="slider" type="range" name="points" min="150" onChange={event => this.onChange(event)} value={this.state.bodyLength} max="200"/>
       </div>
     );
